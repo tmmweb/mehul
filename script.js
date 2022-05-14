e8a242645ac27643cbecfd02b9365513b8bfd75f
tmmweb <76200915+tmmweb@users.noreply.github.com>
2022-05-14 13:50:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Mehul" && b=="abcmehul123")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
