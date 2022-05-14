8bc779e59775d74a850a488b3c5232e2343ac190
tmmweb <76200915+tmmweb@users.noreply.github.com>
2022-05-14 15:03:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var a=prompt("Enter UserName");
  var b=prompt("Enter password");
   	if(a== "Mehul" && b=="abcmehul123")
   	{
-  	    window.location.href="start.html";
+  	    window.location.href="start123start123.html";
   	}
   	else
   	{
